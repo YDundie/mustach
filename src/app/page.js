a00b2478608822ed1a67920b2b3a58f986b71575
Dunder <marko@mustac.dev>
2023-07-28 11:03:51 +0200

    image downscael

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,25 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <img src="./mustach2.png" alt="Mustach" style={{ width: "100%" }} />
-        <h1 className={styles.title}>Embrace challanges, empower solutions</h1>
-        <span className={styles.paragraph}>In progress...</span>
-      </div>
-    </main>
+    <div>
+      <Head>
+        <title>Design Point</title>
+        <meta
+          name="description"
+          content="Mustach custom web development and design"
+        />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <img src="./mustach2.png" alt="Mustach" style={{ width: "100%" }} />
+          <h1 className={styles.title}>
+            Embrace challanges, empower solutions
+          </h1>
+          <span className={styles.paragraph}>In progress...</span>
+        </div>
+      </main>
+    </div>
   );
 }
