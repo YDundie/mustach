5609adace6ff0e7967f104fcf2eec81ca73c4dc1
Dunder <marko@mustac.dev>
2023-07-28 14:36:08 +0200

    added legal

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./page.module.css";
 import Head from "next/head";
 
@@ -20,6 +22,48 @@ export default function Home() {
           <span className={styles.paragraph}>In progress...</span>
         </div>
       </main>
+      <footer>
+        <div className={styles.footer}>
+          <h2>legal</h2>
+          <div className={styles.column}>
+            <div className={styles.row}>
+              <div className={styles.rowEntry}>
+                <h3>HQ</h3>
+                <p>Ivana Zbulja 44, 40326 Sveta Marija</p>
+              </div>
+
+              <div className={styles.rowEntry}>
+                <h3>Country</h3>
+                <p>Croatia</p>
+              </div>
+              <div className={styles.rowEntry}>
+                <h3>County</h3>
+                <p>Međimurje</p>
+              </div>
+              <div className={styles.rowEntry}>
+                <h3>CEO</h3>
+                <p>Marko Mustac</p>
+              </div>
+              <div className={styles.rowEntry}>
+                <h3>IBAN</h3>
+                <p>HR6423400091111230010</p>
+              </div>
+              <div className={styles.rowEntry}>
+                <h3>OIB</h3>
+                <p>Ivana Zbulja 44, 40326 Sveta Marija</p>
+              </div>
+              <div className={styles.rowEntry}>
+                <h3>MBS</h3>
+                <p>Ivana Zbulja 44, 40326 Sveta Marija</p>
+              </div>
+              <div className={styles.rowEntry}>
+                <h3>BANK</h3>
+                <p>Privredna banka Zagreb d. d</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
