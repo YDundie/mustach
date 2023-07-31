f8b122b59f0faf85dccde6df3e487e575284c481
Dunder <marko@mustac.dev>
2023-07-31 17:25:42 +0200

    Update oib and mbs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css";
-import { Inter } from "next/font/google";+
-import { Analytics } from '@vercel/analytics/react';
-
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +13,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Analytics />
-
     </html>
   );
 }
